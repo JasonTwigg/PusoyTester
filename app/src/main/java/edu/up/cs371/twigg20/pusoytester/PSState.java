263a382e16ecdbef8c6f8814656f6e518a4eee52
JasonTwigg <jasontwigg1@gmail.com>
2018-02-25 20:08:02 -0800

    update

--- app/src/main/java/edu/up/cs371/twigg20/pusoytester/PSState.java
+++ app/src/main/java/edu/up/cs371/twigg20/pusoytester/PSState.java
@@ -1,11 +1,16 @@
 package edu.up.cs371.twigg20.pusoytester;
 
 
+import java.util.ArrayList;
+
 /**
  * Created by Jason on 2/25/2018.
  */
 
 public class PSState {
+
+    ArrayList<Card> playerCards = new ArrayList<Card>();
+
 
     //initial constructor
     public void PSState(){
@@ -17,6 +22,15 @@ public class PSState {
 
     //copy constructor
     public void PSState(PSState state, int player){
+
+
+    }
+
+
+
+    @Override
+    public String toString(){
+
 
 
     }
