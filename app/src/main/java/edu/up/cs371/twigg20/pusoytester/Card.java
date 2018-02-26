9726fe2d477b20382de08747ca39c9f8e7d84ee5
JasonTwigg <jasontwigg1@gmail.com>
2018-02-25 19:59:49 -0800

    update

---
+++ app/src/main/java/edu/up/cs371/twigg20/pusoytester/Card.java
@@ -0,0 +1,15 @@
+package edu.up.cs371.twigg20.pusoytester;
+
+/**
+ * Created by Jason on 2/25/2018.
+ */
+
+public class Card {
+
+    public void Card(){
+
+
+
+    }
+
+}
---
+++ app/src/main/java/edu/up/cs371/twigg20/pusoytester/Player.java
@@ -0,0 +1,8 @@
+package edu.up.cs371.twigg20.pusoytester;
+
+/**
+ * Created by Jason on 2/25/2018.
+ */
+
+public class Player {
+}
