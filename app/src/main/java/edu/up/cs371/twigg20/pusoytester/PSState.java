012c83408fe25e0998fcaed12dfc71731b02a808
JasonTwigg <jasontwigg1@gmail.com>
2018-02-25 19:53:28 -0800

    addes psstate

---
+++ app/src/main/java/edu/up/cs371/twigg20/pusoytester/PSState.java
@@ -0,0 +1,24 @@
+package edu.up.cs371.twigg20.pusoytester;
+
+/**
+ * Created by Jason on 2/25/2018.
+ */
+
+public class PSState {
+
+    //initial constructor
+    public void PSState(){
+
+
+
+
+    }
+
+    //copy method
+    public void PSState(){
+
+
+    }
+
+
+}
